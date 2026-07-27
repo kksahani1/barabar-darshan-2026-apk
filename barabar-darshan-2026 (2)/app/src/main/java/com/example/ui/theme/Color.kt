7205c0855004d340c3e958e6723e097563bec611
kksahani1 <145839862+kksahani1@users.noreply.github.com>
2026-07-27 23:26:41 +0530

    Add files via upload

---
+++ barabar-darshan-2026 (2)/app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Natural Tones Light Palette
+val EarthyStonePrimary = Color(0xFF8B7E66)
+val NaturalWarmSand = Color(0xFFF7F2EB)
+val PebbleContainer = Color(0xFFEFEDE7)
+val DeepOliveText = Color(0xFF2D2C28)
+val MutedSlateText = Color(0xFF736F66)
+val SoftBorder = Color(0xFFE5E0D8)
+val DistrictBadgeGold = Color(0xFFD87800)
+
+// Natural Tones Dark Palette
+val EarthyStoneDarkPrimary = Color(0xFFD9D1C5)
+val NaturalDarkSand = Color(0xFF1F1E1B)
+val PebbleDarkContainer = Color(0xFF2A2926)
+val LightOliveText = Color(0xFFF7F2EB)
+
